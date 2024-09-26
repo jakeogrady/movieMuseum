6e9fd45d7b08d5b233e9850c25f8b746cc9220d5
Jake <jakeogrady15@gmail.com>
2024-09-26 20:50:01 +0100

    Initial Commit

---
+++ src/main/java/com/cs5106/movieMuseum/MovieMuseumApplication.java
@@ -0,0 +1,13 @@
+package com.cs5106.movieMuseum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieMuseumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieMuseumApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cs5106/movieMuseum/MovieMuseumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cs5106.movieMuseum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieMuseumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
