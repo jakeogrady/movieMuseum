4057fabfde3a67ca0438b05daab57ff165d82c23
AlexAi4 <axkviv24@gmail.com>
2024-09-30 22:28:20 +0100

    All REST mappings created in MovieController

--- src/main/java/com/cs5106/movieMuseum/domain/web/MovieController.java
+++ src/main/java/com/cs5106/movieMuseum/domain/web/MovieController.java
@@ -2,8 +2,7 @@ package com.cs5106.movieMuseum.domain.web;
 
 import com.cs5106.movieMuseum.domain.Movie;
 import com.cs5106.movieMuseum.domain.repository.MovieRepository;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class MovieController {
@@ -13,8 +12,56 @@ public class MovieController {
         this.movieRepository = movieRepository;
     }
 
-    @GetMapping(value = "/movie")
+    // Consider error handling? (null values mar shampla)
+    // To-Do TESTING!!!!!!!!!!!!!!!!!!!!!!
+    // Get request for all movies returns iterable list
+    @GetMapping(value = "/movies")
     public Iterable<Movie> getMovies() {
         return movieRepository.findAll();
+    }
+
+    @PutMapping(value = "/movies")
+    public void updateMovie(@RequestBody Movie movie) {
+        movieRepository.save(movie);
+    }
+
+    @GetMapping(value = "/movies")
+    public Iterable<Movie> getMovieByTitle(@RequestParam("title") String title) {
+        return movieRepository.findByTitle(title);
+    }
+
+    @GetMapping(value = "/movies")
+    public Iterable<Movie> getMovieByDirector(@RequestParam("director") String director) {
+        return movieRepository.findByDirector(director);
+    }
+
+    @GetMapping(value = "/movies")
+    public Iterable<Movie> getMovieByReleaseYear(@RequestParam("releaseYear") int releaseYear) {
+        return movieRepository.findByReleaseYear(releaseYear);
+    }
+
+    @GetMapping(value = "/movies")
+    public Iterable<Movie> getMovieByImdbRating(@RequestParam("imdbRating") int imdbRating) {
+        return movieRepository.findByImdbRating(imdbRating);
+    }
+
+    @GetMapping(value = "/movies")
+    public Iterable<Movie> getMovieByTitleAndDirector(@RequestParam("title") String title, @RequestParam("director") String director) {
+        return movieRepository.findByTitleAndDirector(title, director);
+    }
+
+    @GetMapping(value = "/movies")
+    public Iterable<Movie> getMovieByTitleAndImdbRatingOrderByImdbRatingDesc(@RequestParam("title") String title, @RequestParam("imdbRating") int imdbRating) {
+        return movieRepository.findByTitleAndImdbRatingOrderByImdbRatingDesc(title, imdbRating);
+    }
+
+    @PostMapping(value = "/movies")
+    public Movie addMovie(@RequestBody Movie movie) {
+        return movieRepository.save(movie);
+    }
+
+    @DeleteMapping(value = "/movies")
+    public void deleteMovie(@RequestBody Movie movie) {
+        movieRepository.delete(movie);
     }
 }
