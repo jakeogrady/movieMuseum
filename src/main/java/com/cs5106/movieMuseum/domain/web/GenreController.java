c1c3550ccdbdf4d0b87c208c3d555b5ca7791667
Alex <axkviv24@gmail.com>
2024-10-01 14:38:43 +0100

    Skeleton of GenreController created

--- src/main/java/com/cs5106/movieMuseum/domain/web/GenreController.java
+++ src/main/java/com/cs5106/movieMuseum/domain/web/GenreController.java
@@ -1,4 +1,19 @@
 package com.cs5106.movieMuseum.domain.web;
 
+import com.cs5106.movieMuseum.domain.Genre;
+import com.cs5106.movieMuseum.domain.repository.GenreRepository;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
 public class GenreController {
+    private final GenreRepository genreRepository;
+
+    public GenreController(GenreRepository genreRepository) {
+        this.genreRepository = genreRepository;
+    }
+
+    @GetMapping(value = "/genres")
+    public Iterable<Genre> getGenres() {
+        return genreRepository.findAll();
+    }
 }
