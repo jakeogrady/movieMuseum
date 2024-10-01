08a325f1752f2e62fe928eee64c51bed13f6a6e8
Alex <axkviv24@gmail.com>
2024-10-01 23:20:33 +0100

    Basic CRUD implemented to GenreController and MovieController, needs fixing

--- src/main/java/com/cs5106/movieMuseum/domain/repository/MovieRepository.java
+++ src/main/java/com/cs5106/movieMuseum/domain/repository/MovieRepository.java
@@ -7,6 +7,7 @@ import com.cs5106.movieMuseum.domain.entity.Movie;
 import java.util.List;
 
 public interface MovieRepository extends CrudRepository<Movie, Long> {
+    List<Movie> findById(int Id);
     List<Movie> findByTitle(String title);
     List<Movie> findByReleaseYear(int releaseYear);
     List<Movie> findByImdbRating(double imdbRating);
