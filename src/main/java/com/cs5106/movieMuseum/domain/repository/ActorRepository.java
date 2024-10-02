0399e0ab96f6a6f538216681e1891a873394fa1a
joeyg <joeygalvin2134@gmail.com>
2024-10-02 11:31:20 +0100

    implemnted 2 custom queries to the actor repo and implemented respective endpoints as per the spec - Actor entity should be ready for first assignment

--- src/main/java/com/cs5106/movieMuseum/domain/repository/ActorRepository.java
+++ src/main/java/com/cs5106/movieMuseum/domain/repository/ActorRepository.java
@@ -1,6 +1,7 @@
 package com.cs5106.movieMuseum.domain.repository;
 
 import com.cs5106.movieMuseum.domain.entity.Actor;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
@@ -9,4 +10,10 @@ public interface ActorRepository extends CrudRepository<Actor, Long> {
     List<Actor> findByFirstName(String firstName);
     List<Actor> findByLastName(String lastName);
     List<Actor> findByFirstNameAndLastName(String firstName, String lastName);
+
+    @Query("SELECT a FROM Actor a WHERE a.firstName LIKE %:firstName%")
+    List<Actor> findByFirstNameSubstring(String firstName);
+
+    @Query("SELECT a FROM Actor a WHERE a.lastName LIKE %:lastName%")
+    List<Actor> findByLastNameSubstring(String lastName);
 }
